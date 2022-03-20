42d53609fdfbee4a68f7531a33ca2a5669080c7b
avonderg <77714730+avonderg@users.noreply.github.com>
2022-03-20 13:36:01 -0400

    finished lab

---
+++ alex-app/src/TextBox.tsx
@@ -0,0 +1,21 @@
+import React, { ChangeEvent } from 'react';
+import { isPropertySignature } from 'typescript';
+
+
+function TextBox(props: {label: string, change : Function}) {
+    const changeHandler = (event : ChangeEvent<HTMLInputElement>) => {
+        props.change(event.target.value)
+    }
+    return (
+        <div className="TextBox">
+            <header className="TextBox-header">
+            </header>
+            <label>
+                {props.label}
+            </label>
+            <input onChange={(e) => changeHandler(e)} type="text"/>
+        </div>
+    );
+}
+
+export default TextBox;
